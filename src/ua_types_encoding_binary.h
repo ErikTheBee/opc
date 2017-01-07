jmrahm/open62541

fix: fix remaining errors for building the full ns0 from nodeset2.xml

src/ua_types_encoding_binary.h
@@ -8,11 +8,11 @@ typedef UA_StatusCode (*UA_exchangeEncodeBuffer)(void *handle, UA_ByteString *bu
 UA_StatusCode UA_EXPORT
 UA_encodeBinary(const void *src, const UA_DataType *type,
                 UA_exchangeEncodeBuffer exchangeBufferCallback, void *exchangeBufferCallbackHandle,
-                UA_ByteString *dst, size_t *offset) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
+                UA_ByteString *dst, size_t *offset);
 
 UA_StatusCode UA_EXPORT
 UA_decodeBinary(const UA_ByteString *src, size_t *offset, void *dst,
-                const UA_DataType *type) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
+                const UA_DataType *type);
 
 size_t UA_calcSizeBinary(void *p, const UA_DataType *type);
 

ErikTheBee/opc

src/ua_types_encoding_binary.h
@@ -8,12 +8,12 @@ typedef UA_StatusCode (*UA_exchangeEncodeBuffer)(void *handle, UA_ByteString *bu
 UA_StatusCode UA_EXPORT
 UA_encodeBinary(const void *src, const UA_DataType *type,
                 UA_exchangeEncodeBuffer exchangeCallback, void *exchangeHandle,
-                UA_ByteString *dst, size_t *offset) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
+                UA_ByteString *dst, size_t *offset);
 
 UA_StatusCode UA_EXPORT
 UA_decodeBinary(const UA_ByteString *src, size_t *offset, void *dst,
                 const UA_DataType *type, size_t customTypesSize,
-                const UA_DataType *customTypes) UA_FUNC_ATTR_WARN_UNUSED_RESULT;
+                const UA_DataType *customTypes);
 
 size_t UA_calcSizeBinary(void *p, const UA_DataType *type);
 